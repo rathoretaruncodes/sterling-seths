6c65eca2ee95a9df567297e775d1817cbb47512e
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-02-09 17:16:57 +0530

    modified env

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const rootRouter = require('./routes/index');
@@ -6,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-const port = 3000;
+const port = (process.env.PORT);
 
 app.use("/api/v1", rootRouter);
 app.listen(port, () => {
