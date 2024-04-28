c18548e8dcabd2cee04752825b4a867cf662a30a
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-04-28 19:50:00 +0530

    Update frontend origin url

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://sterling-seths.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     next();
