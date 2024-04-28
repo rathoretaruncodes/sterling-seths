6339217eb4216da20c0809dfee54f2d4ecaaf242
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-04-28 19:36:47 +0530

    Removed cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const rootRouter = require('./routes/index');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 const port = (process.env.PORT);
 
