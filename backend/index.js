60faa6d90cd21d86d97cc66be81f2fa6945f1340
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-04-28 19:43:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ const app = express();
 
 // app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+  });
 const port = (process.env.PORT);
 
 app.use("/api/v1", rootRouter);
